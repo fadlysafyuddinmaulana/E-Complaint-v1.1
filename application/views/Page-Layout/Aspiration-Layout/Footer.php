42ad0265802b02d34ab030b31f99bfd92333826b
Fadly safyuddin maulana <55474494+fadlysafyuddinmaulana@users.noreply.github.com>
2023-05-01 21:43:31 +0700

    fixed a bug

--- application/views/Page-Layout/Aspiration-Layout/Footer.php
+++ application/views/Page-Layout/Aspiration-Layout/Footer.php
@@ -1,9 +1,16 @@
 <!-- jQuery -->
-<script src="assets/AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url() ?>assets/AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="assets/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>assets/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="assets/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+<script src="<?= base_url() ?>assets/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+<!-- bs-custom-file-input -->
+<script src="<?= base_url() ?>assets/AdminLTE-3.2.0/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<script>
+    $(function() {
+        bsCustomFileInput.init();
+    });
+</script>
 </body>
 
 </html>
