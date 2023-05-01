fd67a91945c574e05fcac74b735b3041f068f13f
Fadly safyuddin maulana <55474494+fadlysafyuddinmaulana@users.noreply.github.com>
2023-05-01 21:43:31 +0700

    Nyicil Project UAS Part san

--- application/views/Starter-Page/Aspiration-Page.php
+++ application/views/Starter-Page/Aspiration-Page.php
@@ -1,45 +1,35 @@
 <div class="register-box">
     <div class="card card-outline card-primary">
         <div class="card-header text-center">
-            <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+            <div class="h1"><b>Complaint</b> Box</div>
         </div>
         <div class="card-body">
-            <p class="login-box-msg">Register a new membership</p>
+            <p class="login-box-msg">Silahkan masukkan keluhan anda.</p>
 
             <form action="#" method="post">
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Full name">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-user"></span>
-                        </div>
-                    </div>
+                <div class="form-group">
+                    <label>NIM</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Masukkan NIM anda">
                 </div>
-                <div class="input-group mb-3">
-                    <input type="email" class="form-control" placeholder="Email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
+                <div class="form-group">
+                    <label>Keluhan Anda</label>
+                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
                 </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
+                <div class="form-group">
+                    <label>Saran Anda</label>
+                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
                 </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Retype password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
+                <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="exampleInputFile">
+                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
                         </div>
                     </div>
-                </div>E_
-                <div class="row">
+                    <span style="text-align: justify;" class="sm-1">Notes:Jika anda punya bukti sebuah foto/dokumen bisa di upload.</span>
+                </div>
+                <div class="row mt-3">
                     <button type="submit" class="btn btn-primary btn-block">Register</button>
                 </div>
             </form>
